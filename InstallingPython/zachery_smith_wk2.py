457501afac2972292f102bec3deff2c6a38437c2
Zac Smith <zrs3141592@gmail.com>
2019-02-15 19:21:24 -0700

    Change file names

---
+++ InstallingPython/zachery_smith_wk2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Zachery Smith ~  zrs3141592@gmail.com
+# Coursera Using Python to Access Web Data
+# Week 1: Installing Python
+
+print("Hello Coursera, I am ready for the world of Python")
