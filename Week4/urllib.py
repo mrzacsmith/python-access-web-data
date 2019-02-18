09cb85707bd6ac8d09e726481d51f1e986609081
Zac Smith <zrs3141592@gmail.com>
2019-02-17 21:32:07 -0700

    Remove file, re-build

--- Week4/urllib.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-__author__ = 'Silverback'
-
-import urllib.request, urllib.parse, urllib.error
-
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-
-for line in fhand:
-    print(line.decode().strip())
