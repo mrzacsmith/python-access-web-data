73d635432a7b557c19983dc1dd135d834fd1bb47
Zac Smith <zrs3141592@gmail.com>
2019-02-17 21:49:47 -0700

    Create script to grab html

--- Week4/urllib3.py
+++ Week4/urllib3.py
@@ -1,6 +1,8 @@
 import urllib.request, urllib.parse, urllib.error
 
-fhand = urllib.request.urlopen('http://data.pr4e.org/page1.htm')
+url = 'http://www.simplesite.com/'
+
+fhand = urllib.request.urlopen(url)
 
 for lines in fhand:
     print(lines.decode().strip())
