a2f9e3ec02d66c507bc689a281f3285bc8b1071e
Zac Smith <zrs3141592@gmail.com>
2019-02-18 19:33:01 -0700

    Solution to week 4

--- Week4/solutionWK4.py
+++ Week4/solutionWK4.py
@@ -4,3 +4,13 @@ from bs4 import BeautifulSoup
 url = input('Enter URL - ')
 html = urllib.request.urlopen(url).read()
 
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup('span')
+listSum = []
+
+
+for tag in tags:
+    listSum.append(int(tag.string))
+
+print('Sum: ' + str(sum(listSum)))
