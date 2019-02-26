08647e17df5c2fbb10f1dbf553f1467f2e06fd5a
Zac Smith <zrs3141592@gmail.com>
2019-02-25 20:16:27 -0700

    Solution for weekly assignment

--- Week4/solutionWK4links.py
+++ Week4/solutionWK4links.py
@@ -1,5 +1,25 @@
-import urllib.request, urllib.parse, urllib.error
-from bs4 import BeautifulSoup
 
-url = input('Enter URL - ')
-html = urllib.request.urlopen(url, 'html.parser')
+from bs4 import BeautifulSoup
+import ssl
+import urllib.request
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter your start url: ')
+count = input('Enter your count: ')
+position = input('Enter your position: ')
+
+for i in range(int(count)):
+	html = urllib.request.urlopen(url).read()
+	soup = BeautifulSoup(html, 'html.parser')
+	tags = soup('a')
+	number = 0
+	for tag in tags:
+		number += 1
+		if number == int(position):
+			url = tag.get('href')
+			if i == int(count)-1:
+				print(tag.contents[0])
+			break
