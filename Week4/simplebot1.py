8b07ce5023f1a5c7fbed541cde93e2ee6121fb38
Zac Smith <zrs3141592@gmail.com>
2019-02-26 22:35:59 -0700

    Add .format

--- Week4/simplebot1.py
+++ Week4/simplebot1.py
@@ -2,11 +2,14 @@
 
 responses = {
     "what's your name?" : "my name is Echobot",
-    "what's the weather today?" : "it is sunny"
+    "what's the weather today?" : "it's {} today"
 }
+
+weather_today = "cloudy"
 
 def respond(message):
     if message in responses:
-        print(responses[message])
+        print(responses[message].format(weather_today))
 
+respond("what's the weather today?")
 respond("what's your name?")
