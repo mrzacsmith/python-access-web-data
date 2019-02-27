47f731c22355b3b7b6592ceb0dd4a9a12039bc07
Zac Smith <zrs3141592@gmail.com>
2019-02-26 23:10:36 -0700

    Add random to responses

--- Week4/simplebot1.py
+++ Week4/simplebot1.py
@@ -1,15 +1,20 @@
-
+import random
 
 responses = {
-    "what's your name?" : "my name is Echobot",
-    "what's the weather today?" : "it's {} today"
+    "what's your name?" : [
+      "my name is Echobot",
+      "they call be Mr. Echobot",
+      "the names Bot, Echobot"
+      ]
+
+    # "what's the weather today?" : "it's {} today"
 }
 
 weather_today = "cloudy"
 
 def respond(message):
     if message in responses:
-        print(responses[message].format(weather_today))
+        print(random.choice(responses[message]))
 
-respond("what's the weather today?")
+# respond("what's the weather today?")
 respond("what's your name?")
