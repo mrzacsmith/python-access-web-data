ffb5148cb4151b99768005d6373d740fa4ffce78
Zac Smith <zrs3141592@gmail.com>
2019-03-04 11:40:05 -0700

    Add .encode() and close()

--- BlackHatPython/tcp_client.py
+++ BlackHatPython/tcp_client.py
@@ -5,7 +5,14 @@ target_port = 80
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n").encode
+
+# Must encode
+cmd = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n".encode()
+client.send(cmd)
+
 response = client.recv(4096)
 
 print(response)
+
+# Close connection
+client.close()
