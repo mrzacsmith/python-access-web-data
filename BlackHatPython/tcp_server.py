1ae37a1c1aa544777499e5539b9edb5715e32832
Zac Smith <zrs3141592@gmail.com>
2019-03-04 16:50:56 -0700

    Create simple tcp server

---
+++ BlackHatPython/tcp_server.py
@@ -0,0 +1,14 @@
+import socket
+import threading 
+
+bind_ip = '0.0.0.0'
+bind_port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip, bind_port))
+
+server.listen(5)
+
+print('[*] Listening on %s:%d' % (bind_ip, bind_port))
+
