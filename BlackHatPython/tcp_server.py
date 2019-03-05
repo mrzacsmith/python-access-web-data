e817c4302786465d58773b0d6d86dfb2a6e050a6
Zac Smith <zrs3141592@gmail.com>
2019-03-04 19:38:09 -0700

    Add to server code

--- BlackHatPython/tcp_server.py
+++ BlackHatPython/tcp_server.py
@@ -12,3 +12,12 @@ server.listen(5)
 
 print('[*] Listening on %s:%d' % (bind_ip, bind_port))
 
+def handle_client(client_socket):
+    request = client_socket.recv(1024)
+    print('[*] Received: %s' % request)
+    client_socket.send('ACK!')
+    client_socket.close()
+
+while True:
+    client, addr = server.accept()
+    
