ea589f4ce1c3f551aade8781a41ad05facdd35e5
Zac Smith <zrs3141592@gmail.com>
2019-03-05 20:44:10 -0700

    Layout def

---
+++ BlackHatPython/netcat.py
@@ -0,0 +1,21 @@
+import sys
+import socket
+import getopt
+import threading
+import subprocess
+
+listen  = False
+command = False
+upload  = ""
+execute = ""
+target  = ""
+upload_destination = ""
+port    = 0
+
+def usage():
+
+
+def main():
+
+
+main()
